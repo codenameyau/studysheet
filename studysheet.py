0cdfaff3c2355e188dd44067c63176362589b16a
Jorge Yau <jyau01@citymail.cuny.edu>
2014-04-05 00:38:35 -0400

    Adding studysheet main with wordlist

---
+++ studysheet.py
@@ -0,0 +1,20 @@
+"""
+StudySheet - Simple study sheets
+
+Tasks:
+1. Integrate with WolframAlpha API
+2. Save results as a pdf
+"""
+import requests
+
+
+def main():
+    """
+    Reads content of 'wordlist.txt' and generates a file
+    containing definitions of concepts in 'output/'
+    """
+    print "Hello StudySheet"
+
+
+if __name__ == '__main__':
+    main()
