e3c42fcf53ec9fc4ce955be18f6bd0fb9060607d
Jorge Yau <jyau01@citymail.cuny.edu>
2014-04-06 11:54:04 -0400

    Adding transcriber for word documents

---
+++ knowledge/transcriber.py
@@ -0,0 +1,14 @@
+"""
+knowledge: transcriber.py
+
+Module for transcribing and organizing information
+and images into word and PDF documents.
+"""
+
+def generate_doc(content, source):
+    """
+    Public: (List, String) -> None
+
+    Creates a word document, formatted based on source.
+    """
+    print content
