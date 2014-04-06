ce709fdbda1425e7edc0ec7e9fa643d1c372fd4e
Jorge Yau <jyau01@citymail.cuny.edu>
2014-04-06 12:19:22 -0400

    Adding formatting for wikipedia documents

--- knowledge/transcriber.py
+++ knowledge/transcriber.py
@@ -1,9 +1,12 @@
 """
 knowledge: transcriber.py
 
-Module for transcribing and organizing information
-and images into word and PDF documents.
+Module for transcribing and organizing information and
+images into word and PDF documents.
 """
+import sys
+import docx
+
 
 def generate_doc(content, source):
     """
@@ -11,4 +14,22 @@ def generate_doc(content, source):
 
     Creates a word document, formatted based on source.
     """
+    if source in WORD_FORMATTING:
+        WORD_FORMATTING[source](content)
+    else:
+        sys.exit('Formatting for %s is missing' % source)
+
+
+def _format_wikipedia_doc(content):
+    """
+    Private: (List) -> None
+
+    Generates document with custom wikipedia formatting.
+    """
+    document = docx.Document()
     print content
+
+
+WORD_FORMATTING = {
+    'wikipedia' : _format_wikipedia_doc
+}
